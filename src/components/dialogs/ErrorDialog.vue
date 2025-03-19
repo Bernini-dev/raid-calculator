6f62f3a95bfc9b52dec6ec3aa8223c1512d72310
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-03-19 11:06:03 -0300

    agrega componente ErrorDialog para mostrar mensajes de error en la aplicación

---
+++ src/components/dialogs/ErrorDialog.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const { error } = defineProps(["error"]);
+</script>
+
+<template>
+  <div class="flex justify-center content-center">
+    <div class="bg-red-500/65 text-white p-6 rounded-xl font-bold w-xl">
+      {{ error }}
+    </div>
+  </div>
+</template>
