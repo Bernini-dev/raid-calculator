234290421a0a388d8f825baddeb048cd940e4771
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-03-19 11:06:46 -0300

    agrega el componente ErrorDialog en App.vue para mostrar mensajes de error

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { ref, type Ref } from "vue";
 import RaidForm from "@/components/forms/RaidForm.vue";
 import ResultTable from "@/components/ResultTable.vue";
 import type { RAIDResult } from "@/tools/calculator";
+import ErrorDialog from "@/components/dialogs/ErrorDialog.vue";
 
 const result: Ref<RAIDResult> = ref({
   totalSize: 0,
@@ -27,4 +28,5 @@ const updateResult = (newResult: RAIDResult) => {
       :faultTolerance="result.faultTolerance"
     />
   </div>
+  <ErrorDialog v-if="result.error !== ''" :error="result.error" />
 </template>
